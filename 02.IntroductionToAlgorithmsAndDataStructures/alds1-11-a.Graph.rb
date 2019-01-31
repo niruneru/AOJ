c888c7d08d7c2bf53a67aaa08c0d4304b1adbd6b
niruneru <niruneru@outlook.com>
2019-01-31 12:59:12 +0000

    深さ優先探索のコーディング完了

--- 02.IntroductionToAlgorithmsAndDataStructures/alds1-11-a.Graph.rb
+++ 02.IntroductionToAlgorithmsAndDataStructures/alds1-11-a.Graph.rb
@@ -1,18 +1,25 @@
-row = []
-matrix = []
-
+#-- ノードの数を取得
 n = gets.chomp.to_i
-[*0...n].each { |i| row[i] = 0 }
-[*0...n].each { |i| matrix[i] = row.clone }
-p matrix
-n.times {
+
+#-- まずはゼロで埋める
+#-- 最初はこう↓したけど
+# row = []
+# matrix = []
+# [*0...n].each { |i| row[i] = 0 }
+# [*0...n].each { |i| matrix[i] = row.clone }
+#-- Array.newで配列を作る方法のほうが簡潔。これだとrowがブロックになって別オブジェクトになるので更新しても大丈夫。
+#-- matrix = Array.new n { Array.new n, 0 }
+#-- でも、これ↓に落ち着いた
+matrix = Array.new(n) { [0] * n }
+
+n.times do
     xs = gets.chomp.split.map(&:to_i)
     num = xs.shift; num -= 1; xs.shift
     xs.each { |x| matrix[num][x - 1] = 1 }
-}
+end
 
-[*0...n].each { |i|
+[*0...n].each do |i|
     if matrix[i] != nil
-        puts matrix[i].join(" ")
+        puts matrix[i].join(' ')
     end
-}
+end
--- 02.IntroductionToAlgorithmsAndDataStructures/alds1-11-b.Depth-First-Search.rb
+++ 02.IntroductionToAlgorithmsAndDataStructures/alds1-11-b.Depth-First-Search.rb
@@ -1,29 +1,32 @@
-#-- DFS
+#-- alds1-11-b :: DepthFirstSearch
+#----------------------------------------------
 
-def search(x, mtx, ds)
+#-- input : 訪問中のノード、隣接行列、発見時刻リスト
+#-- output: 訪問中のノードに隣接しているが
+#--         まだ発見されていないノード番号（見つからない場合はnil）
+def search(x, matrix, ds)
     rc = nil
-    mtx[x].each_with_index { |a, i|
-        if a && ds[x] != nil
+    matrix[x].each_with_index do |a, i|
+        if a && ds[i] == nil
             rc = i
             break
         end
-    }
+    end
+    rc
 end
-ds = [] #-- 発見時刻
-fs = [] #-- 完了時刻
+
+ds = [] #-- 発見時刻(d:discovery)
+fs = [] #-- 完了時刻(f:fixed)
 tm = 0  #-- 現在時刻
 
 #-- 隣接行列で格納
-mtx = []
-row = []
 n = gets.chomp.to_i
-[*0...n].each { |i| row[i] = false }
-[*0...n].each { |i| mtx[i] = row.clone }
-n.times {
+matrix = Array.new(n) { [false] * n }
+n.times do
     xs = gets.chomp.split.map(&:to_i)
     num = xs.shift; num -= 1; xs.shift
-    xs.each { |x| mtx[num][x - 1] = true }
-}
+    xs.each { |x| matrix[num][x - 1] = true }
+end
 
 #-- 深さ探索用スタック
 stack = []
@@ -34,19 +37,20 @@ tm += 1; ds[x] = tm
 stack << x
 
 while stack != []
-    x = stack[-1]
-    y = search(x, mtx, ds) #-- xの次の隣接ノードを取得
+    x = stack.last #-- stack[-1]でも取れる
+    y = search(x, matrix, ds) #-- xの次の隣接ノードを取得
 
-    p y
-
+    #-- 隣接ノードが見つからなかったらそのノードの探索完了
     if y == nil
         tm += 1; fs[x] = tm
         stack.pop
+    #-- 見つかったならそれをスタックに積んで発見時刻を記録
     else
         stack << y
         tm += 1; ds[y] = tm
     end
 end
 
-p ds
-p fs
+[*0...n].each do |i|
+  puts "#{i+1} #{ds[i]} #{fs[i]}"
+end
